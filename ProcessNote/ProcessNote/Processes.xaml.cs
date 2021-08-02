871e154edd7e7561ed1565114b9d079fd0e84e70
Whentheknight <66561640+Whentheknight@users.noreply.github.com>
2021-08-02 15:03:13 +0200

    Write ListView to Processes.xaml

--- ProcessNote/ProcessNote/Processes.xaml.cs
+++ ProcessNote/ProcessNote/Processes.xaml.cs
@@ -16,11 +16,11 @@ using System.Windows.Shapes;
 namespace ProcessNote
 {
     /// <summary>
-    /// Interaction logic for Page1.xaml
+    /// Interaction logic for Processes.xaml
     /// </summary>
-    public partial class Page1 : Page
+    public partial class Processes : UserControl
     {
-        public Page1()
+        public Processes()
         {
             InitializeComponent();
         }
