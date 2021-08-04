c0af255388b6110e3b1887f53699dc52cd5ea2f8
natekolozsvari <nate.kolozsvari@gmail.com>
2021-08-04 18:57:22 +0200

    Create CommentModal to show comments for a process Create Comment class Implement ability for user to add comments for a process

---
+++ ProcessNote/ProcessNote/CommentModal.xaml.cs
@@ -0,0 +1,50 @@
+﻿using ProcessNote.Entities;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ProcessNote
+{
+    /// <summary>
+    /// Interaction logic for Comment.xaml
+    /// </summary>
+    public partial class CommentModal : Window
+    {
+        private int _id;
+        public String ProcessName { get; private set; }
+
+        public CommentCollection Comments { get; private set; }
+
+        public CommentModal()
+        {
+            InitializeComponent();
+        }
+
+        public CommentModal(int id, CommentCollection comments)
+        {
+            InitializeComponent();
+            DataContext = this;
+            _id = id;
+            ProcessName = Process.GetProcessById(id).ProcessName;
+            Comments = comments;
+        }
+            
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var newComment = new Comment(CommentTextBox.Text, DateTime.Now);
+            Comments.Add(newComment);
+            CommentTextBox.Clear();
+        }
+    }
+}
